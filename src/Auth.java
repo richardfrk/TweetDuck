31b5d0a3ec34b0ff5e56ff895af5358ba0f342ba
richardfrk <richardfrk@d40950d0-2123-b8c3-f4c7-bc0e296006d1>
2010-09-01 19:43:15 +0000

    Novos arquivos

---
+++ src/Auth.java
@@ -0,0 +1,150 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * Auth.java
+ *
+ * Created on 18/08/2010, 14:29:52
+ */
+
+package tweetduck;
+
+/**
+ *
+ * @author Richard Frank
+ */
+
+import javax.swing.JOptionPane;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class Auth extends javax.swing.JFrame {
+
+    /** Creates new form Auth */
+    public Auth() {
+        initComponents();
+
+    }
+
+    public String getPass() {
+	String senha = new String (psPass.getPassword());
+        return senha;
+}
+
+public void Connect() {
+	String usuario = txtLogin.getText();
+	String senha = getPass();
+
+	try {
+		Twitter t = new TwitterFactory().getInstance(usuario, senha);
+                t.get
+
+                Base b = new Base(t);
+                b.setLocationRelativeTo(null);
+		b.setVisible(true);
+ 
+		this.dispose();
+	} catch (TwitterException ex) {
+                        if(ex.getStatusCode() == 401){
+			JOptionPane.showMessageDialog(this, "Opss!, your username or password are incorrect. Try again!");
+                        }
+		}
+	}
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbLogin = new javax.swing.JLabel();
+        txtLogin = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        psPass = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("TweetBird - Login");
+        setBackground(new java.awt.Color(255, 255, 255));
+        setForeground(new java.awt.Color(255, 255, 255));
+        setResizable(false);
+
+        lbLogin.setForeground(new java.awt.Color(102, 102, 102));
+        lbLogin.setText("Username");
+
+        txtLogin.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabel1.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel1.setText("Password");
+
+        psPass.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
+        jButton1.setForeground(new java.awt.Color(51, 51, 51));
+        jButton1.setText("Sign in");
+        jButton1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbLogin)
+                    .addComponent(jLabel1)
+                    .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(psPass, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lbLogin)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(psPass, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        Connect();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String args[]) {
+        //java.awt.EventQueue.invokeLater(new Runnable() {
+            //public void run() {
+                //new Auth().setVisible(true);
+            //}
+        //});
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lbLogin;
+    private javax.swing.JPasswordField psPass;
+    private javax.swing.JTextField txtLogin;
+    // End of variables declaration//GEN-END:variables
+
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tweetduck;
+
+/**
+ *
+ * @author Richard Frank
+ */
+public class Main {
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Auth a = new Auth();
+        a.setLocationRelativeTo(null);
+        a.setVisible(true);
+    }
+}
