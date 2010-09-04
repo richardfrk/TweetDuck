189a69389fa7d83371b35bd9c5c6dc615c006d3a
richardfrk <richardfrk@d40950d0-2123-b8c3-f4c7-bc0e296006d1>
2010-09-04 19:13:18 +0000

    New Authorization Mode

--- src/Auth.java
+++ src/Auth.java
@@ -17,7 +17,6 @@
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -26,61 +25,46 @@ import twitter4j.http.RequestToken;
 
 public class Auth extends javax.swing.JFrame {
 
+    Twitter t;
+    String token;
+    String tokenSecret;
+    String PIN;
+    String requestPIN;
+
     /** Creates new form Auth */
     public Auth() {
+
+        try {
+
+                t = new TwitterFactory().getInstance();
+                t.setOAuthConsumer("UwUkPU4p5KWMrUNvFRNcUg","dEVSuTUbJFRQ79V0DkqbKbryidR5l5mWvxZgw16cv8");
+                RequestToken requestToken = t.getOAuthRequestToken();
+                token = requestToken.getToken();
+                tokenSecret = requestToken.getTokenSecret();
+
+                String requestPIN = requestToken.getAuthorizationURL();
+                java.awt.Desktop.getDesktop().browse(java.net.URI.create(requestPIN));
+
+        } catch (TwitterException ex) {
+            Logger.getLogger(Auth.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Auth.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+
         initComponents();
 
     }
-
-    public String getPass() {
-	String senha = new String (psPass.getPassword());
-        return senha;
-}
-
-public void Connect() {
-
-        //String usuario = txtLogin.getText();
-	//String senha = getPass();
-
-	try {
-
-		Twitter t = new TwitterFactory().getInstance();
-                t.setOAuthConsumer("UwUkPU4p5KWMrUNvFRNcUg","dEVSuTUbJFRQ79V0DkqbKbryidR5l5mWvxZgw16cv8");
-                RequestToken requestToken = t.getOAuthRequestToken();
-                String token = requestToken.getToken();
-                String tokenSecret = requestToken.getTokenSecret();
- 
-                String requestPIN = requestToken.getAuthorizationURL();
-                java.awt.Desktop.getDesktop().browse(java.net.URI.create(requestPIN));
-                String PIN = JOptionPane.showInputDialog("Enter the PIN");
-
-                AccessToken accessToken = t.getOAuthAccessToken(token, tokenSecret, PIN);
-                t.setOAuthAccessToken(accessToken);
-                
-                Base b = new Base(t);
-                b.setLocationRelativeTo(null);
-		b.setVisible(true);
- 
-		this.dispose();
-                
-	} catch (IOException ex) {
-            Logger.getLogger(Auth.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (TwitterException ex) {
-                        if(ex.getStatusCode() == 401){
-			JOptionPane.showMessageDialog(this, "Opss!, your username or password are incorrect. Try again!");
-                        }
-		}
-	}
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         lbLogin = new javax.swing.JLabel();
-        txtLogin = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        psPass = new javax.swing.JPasswordField();
+        txtPIN = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("TweetDuck - Login");
@@ -89,14 +73,9 @@ public void Connect() {
         setResizable(false);
 
         lbLogin.setForeground(new java.awt.Color(102, 102, 102));
-        lbLogin.setText("Username");
+        lbLogin.setText("Enter the PIN");
 
-        txtLogin.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-
-        jLabel1.setForeground(new java.awt.Color(102, 102, 102));
-        jLabel1.setText("Password");
-
-        psPass.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        txtPIN.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
         jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setForeground(new java.awt.Color(51, 51, 51));
@@ -108,61 +87,100 @@ public void Connect() {
             }
         });
 
+        jLabel1.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel1.setText("Welcome to");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel2.setText("TweetDuck");
+
+        jLabel3.setForeground(new java.awt.Color(0, 102, 204));
+        jLabel3.setText("Get PIN?");
+        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel3MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbLogin)
-                    .addComponent(jLabel1)
-                    .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(psPass, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(107, 107, 107)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lbLogin)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 143, Short.MAX_VALUE)
+                        .addComponent(jLabel3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(jLabel2))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(txtPIN, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lbLogin)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(psPass, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbLogin)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtPIN, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(13, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        Connect();
+        try {
+            // TODO add your handling code here:
+            PIN = txtPIN.getText();
+            AccessToken accessToken = t.getOAuthAccessToken(token, tokenSecret, PIN);
+            t.setOAuthAccessToken(accessToken);
+            t.verifyCredentials();
+            
+            Base b = new Base(t);
+            b.setLocationRelativeTo(null);
+            b.setVisible(true);
+            this.dispose();
+        } catch (TwitterException ex) {
+            Logger.getLogger(Auth.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        //java.awt.EventQueue.invokeLater(new Runnable() {
-            //public void run() {
-                //new Auth().setVisible(true);
-            //}
-        //});
-    }
+    private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
+        try {
+            // TODO add your handling code here:
+            java.awt.Desktop.getDesktop().browse(java.net.URI.create(requestPIN));
+        } catch (IOException ex) {
+            Logger.getLogger(Auth.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jLabel3MouseClicked
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel lbLogin;
-    private javax.swing.JPasswordField psPass;
-    private javax.swing.JTextField txtLogin;
+    private javax.swing.JTextField txtPIN;
     // End of variables declaration//GEN-END:variables
 
 }
